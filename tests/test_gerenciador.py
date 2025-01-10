0d2a3b2afd75302f6c0554997ddc8a59a0dec75c
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-01-10 16:48:22 -0300

    Adiciona recursos de listar tarefas

---
+++ tests/test_gerenciador.py
@@ -0,0 +1,84 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from gerenciador_tarefas.gerenciador import TAREFAS, app
+
+
+def test_list_task_status_200():
+    client = TestClient(app)
+    response = client.get("/tarefas")
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_list_taks_format_json():
+    client = TestClient(app)
+    response = client.get("/tarefas")
+    assert response.headers["Content-Type"] == "application/json"
+
+
+def test_list_task_list():
+    client = TestClient(app)
+    response = client.get("/tarefas")
+    assert isinstance(response.json(), list)
+
+
+def test_when_task_return_id():
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
+    client = TestClient(app)
+    response = client.get("/tarefas")
+    assert "id" in response.json().pop()
+    TAREFAS.clear()
+
+
+def test_when_task_return_title():
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
+    client = TestClient(app)
+    response = client.get("/tarefas")
+    assert "titulo" in response.json().pop()
+    TAREFAS.clear()
+
+
+def test_when_task_return_description():
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
+
+    client = TestClient(app)
+    response = client.get("/tarefas")
+    assert "descricao" in response.json().pop()
+    TAREFAS.clear()
+
+
+def test_when_task_return_state():
+    TAREFAS.append(
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+    )
+
+    client = TestClient(app)
+    response = client.get("/tarefas")
+    assert "estado" in response.json().pop()
+    TAREFAS.clear()
